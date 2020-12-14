333146491266bf163162770251898ea5ce0eeab5
Gerard Bentley <gerardrbentley@gmail.com>
2020-12-14 17:46:56 +0000

    refactor: Move dark mode logic to hook

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,29 @@
 <template>
-  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
-  <button class="btn example" @click="toggleDarkMode">Toggle Dark Mode</button>
-  <HelloWorld msg="Hello Vue 3.0 + Vite + Tailwind 2.0" />
+	<img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
+	<button data-test="dark-mode" class="btn example" @click="toggleDarkMode">
+		Toggle Dark Mode
+	</button>
+	<HelloWorld msg="Hello Vue 3.0 + Vite + Tailwind 2.0" />
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineComponent } from "vue";
+import useTailwindDarkMode from "./tailwind-dark-mode";
+import HelloWorld from "./components/HelloWorld.vue";
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  setup () {
-    onBeforeMount(() => {
-      // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-      if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-	document.querySelector('html').classList.add('dark')
-      } else {
-	document.querySelector('html').classList.remove('dark')
-      }
-     })
-
-
-    // Whenever the user explicitly chooses to respect the OS preference
-    // localStorage.removeItem('theme')
-    const toggleDarkMode = () => {
-      // Whenever the user explicitly chooses light mode
-      if (document.querySelector('html').classList.contains('dark')) {
-	document.querySelector('html').classList.remove('dark')
-	localStorage.theme = 'light'
-      } else {
-	// Whenever the user explicitly chooses dark mode
-	localStorage.theme = 'dark'
-	document.querySelector('html').classList.add('dark')
-      }
-    }
-    return {toggleDarkMode}
-    }
-})
+	name: "App",
+	components: {
+		HelloWorld,
+	},
+	setup() {
+		const { toggleDarkMode } = useTailwindDarkMode();
+		return { toggleDarkMode };
+	},
+});
 </script>
 
 <style>
- .example {
-   @apply border-4 border-green-600 dark:border-red-600 shadow-lg;
- }
+.example {
+	@apply border-4 border-green-600 dark:border-red-600 shadow-lg;
+}
 </style>
