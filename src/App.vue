e52051ed1d8f31dc0151fdd54bccce6ec1e4a3f1
Gerard Bentley <gerardrbentley@gmail.com>
2020-12-14 18:06:07 +0000

    chore: lint App.vue...

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-	<img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
-	<button data-test="dark-mode" class="btn example" @click="toggleDarkMode">
-		Toggle Dark Mode
-	</button>
-	<HelloWorld msg="Hello Vue 3.0 + Vite + Tailwind 2.0" />
+  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
+  <button data-test="dark-mode" class="btn example" @click="toggleDarkMode">
+    Toggle Dark Mode
+  </button>
+  <HelloWorld msg="Hello Vue 3.0 + Vite + Tailwind 2.0" />
 </template>
 
 <script lang="ts">
@@ -12,20 +12,20 @@ import useTailwindDarkMode from './tailwind-dark-mode';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-	name: 'App',
-	components: {
-		HelloWorld,
-	},
-	setup() {
-		const {toggleDarkMode} = useTailwindDarkMode();
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const {toggleDarkMode} = useTailwindDarkMode();
 
-		return {toggleDarkMode};
-	},
+    return {toggleDarkMode};
+  },
 });
 </script>
 
 <style>
 .example {
-	@apply border-4 border-green-600 dark:border-red-600 shadow-lg;
+  @apply border-4 border-green-600 dark:border-red-600 shadow-lg;
 }
 </style>
